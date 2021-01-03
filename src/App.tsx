ae41cc4cca65b234e4cb3900e782f1582c21f581
Emre Yildirim <temreyildirim8@gmail.com>
2021-01-03 19:33:09 +0300

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+// tslint:disable: jsx-no-lambda
+
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/' component={Dashboard} exact={true} />
+        <Route path='/default' render={() => <Redirect to='/' />} />
+      </Switch>
     </div>
   );
 }
